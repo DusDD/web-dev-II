40cc582650d641663137922f76316a11e5b18935
nikla <niklas.brueckner10@gmail.com>
2024-04-24 16:10:22 +0200

    included login.html into index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-echo Convo ist der geilste Messenger!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Convo Messenger</title>
+</head>
+<body>
+<form action="authenticate.php" method="post">
+    <label>
+        Username:
+        <input type="text" name="name" placeholder="Your Name" required>
+    </label>
+    <label>
+        Password:
+        <input type="password" name="password" placeholder="Your Password" required>
+    </label>
+    <button type="submit" name="action" value="login">Login</button>
+    <button type="submit" name="action" value="register">Register</button>
+</form>
+</body>
+</html>
 ?>
