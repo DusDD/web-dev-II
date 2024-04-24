adcbb09fe7beb8c352e06a85cf5c5b3e6a4f3d7c
nikla <niklas.brueckner10@gmail.com>
2024-04-24 16:11:25 +0200

    nn

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Convo Messenger</title>
-</head>
-<body>
-<form action="authenticate.php" method="post">
-    <label>
-        Username:
-        <input type="text" name="name" placeholder="Your Name" required>
-    </label>
-    <label>
-        Password:
-        <input type="password" name="password" placeholder="Your Password" required>
-    </label>
-    <button type="submit" name="action" value="login">Login</button>
-    <button type="submit" name="action" value="register">Register</button>
-</form>
-</body>
-</html>
+include login.html
 ?>
