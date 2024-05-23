3f6df0057df3f5f3e79d600a7e5f410513189751
vclume <146815299+vclume@users.noreply.github.com>
2024-05-23 19:02:20 +0200

    logout.php changes

--- public/logout.php
+++ public/logout.php
@@ -4,6 +4,10 @@
 if (session_status() == PHP_SESSION_ACTIVE) {
     session_unset();
     session_destroy();
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     header("Location: /login.html");
     exit();
 }
+
