8f9b3f98a33b3c50ae31ed061da892eed58ae845
Dominik <domingob@gmx.de>
2024-05-26 18:17:47 +0200

    added start session

--- public/logout.php
+++ public/logout.php
@@ -1,12 +1,15 @@
 <?php
+session_start();
 
-// Make sure the user is really logged in
-if (session_status() == PHP_SESSION_ACTIVE) {
+// Check if the user is logged in
+if (isset($_SESSION['user_id'])) {    
     $_SESSION  = array();
     session_destroy();
+    
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
+    
     header("Location: /login.html");
     exit();
 }
