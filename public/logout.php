8b5bc086b50b113a659fd4831ed03b5c9d94fa41
Dominik <domingob@gmx.de>
2024-05-26 17:53:22 +0200

    changed to $_SESSION = array;

--- public/logout.php
+++ public/logout.php
@@ -2,7 +2,7 @@
 
 // Make sure the user is really logged in
 if (session_status() == PHP_SESSION_ACTIVE) {
-    session_unset();
+    $_SESSION  = array
     session_destroy();
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
