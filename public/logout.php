ece4baf0fdcbede17bf92ec38a8c22bb1d5f5704
Dominik <domingob@gmx.de>
2024-05-27 11:17:01 +0200

    shorted logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 
+
 // Check if the user is logged in
 if (isset($_SESSION['user_id'])) {    
     $_SESSION  = array();
     session_destroy();
-    
-    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
     
     header("Location: /login.html");
     exit();
