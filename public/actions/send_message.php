cb9a7158da0b6ea0d0ae92fd26785d8912b32c2c
Dominik <domingob@gmx.de>
2024-05-29 14:20:24 +0200

    changed mor chat to chats

--- public/actions/send_message.php
+++ public/actions/send_message.php
@@ -27,8 +27,8 @@ $db = Database::getConnection();
 $current_datetime = date('Y-m-d H:i:s');
 
 // Insert the new message into the database
-$query = $db->prepare("INSERT INTO messages (chat_id, sender_id, content, sent_date) VALUES (?, ?, ?, ?)");
-$query->bind_param('iiss', $_POST["chat_id"], $_SESSION["user_id"], $_POST["message"], $current_datetime);
+$query = $db->prepare("INSERT INTO messages (chats_id, sender_id, content, sent_date) VALUES (?, ?, ?, ?)");
+$query->bind_param('iiss', $_POST["chats_id"], $_SESSION["user_id"], $_POST["message"], $current_datetime);
 if (!$query->execute()) {
     exit("Error creating new message!");
 }
@@ -36,7 +36,7 @@ $query->close();
 
 // Update chat last_active attribute
 $query = $db->prepare("UPDATE chats SET last_active = ? WHERE id = ?");
-$query->bind_param('si', $current_datetime, $_POST["chat_id"]);
+$query->bind_param('si', $current_datetime, $_POST["chats_id"]);
 if (!$query->execute()) {
     exit("Error updating chat last_active!");
 }
