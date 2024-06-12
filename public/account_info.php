d63f5a18dbb13436439779569fb661ee2fc404d2
lukcod3 <lukas.fausten@web.de>
2024-06-12 10:48:47 +0200

    added login check in account_info.php

--- public/account_info.php
+++ public/account_info.php
@@ -1,5 +1,16 @@
 
 <?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Redirect user to login page if no login data is set
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+    header("Location: /login.html");
+    exit();
+}
+
 include "main_menue.html";
 include "account_info.html";
 ?>
